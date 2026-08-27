1ad5389d160d78c7c490c03790fc237841255acb
Douglas <douglas9525@gmail.com>
2026-08-27 19:11:16 +0800

    updated ex00

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,14 +6,15 @@
 /*   By: kong <kong@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 17:31:47 by kong              #+#    #+#             */
-/*   Updated: 2026/08/22 18:51:40 by kong             ###   ########.fr       */
+/*   Updated: 2026/08/27 19:09:33 by kong             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
-# include "Zombie.h"
+# include <string>
+# include <iostream>
 
 class Zombie
 {
